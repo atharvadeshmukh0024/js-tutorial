3b8dcc015aeaad9479c60d063804f3335ec3ccae
Atharva Mangesh Deshmukh <atharvadeshmukh0024@gmail.com>
2025-08-02 06:36:23 +0000

    conversion of datatypes

--- 01_basic/variable.js
+++ 01_basic/variable.js
@@ -2,3 +2,16 @@ const accId =14453
 let accEmail ="a@gmail.com"
 var accPass ="12345"
 accountCity ="Jaipur"
+
+console.log(typeof accEmail);
+
+let email =Number(accEmail)
+
+console.log(typeof email);
+
+
+let isLog =0
+
+let isLogBol =Boolean(isLog)
+
+console.log(isLogBol)
