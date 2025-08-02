a5ed5752836091786d4706fe0bd34f9c81d2214b
Atharva Mangesh Deshmukh <atharvadeshmukh0024@gmail.com>
2025-08-02 06:54:58 +0000

    postfix ,prefix String Conversion

--- 01_basic/variable.js
+++ 01_basic/variable.js
@@ -1,17 +1,49 @@
-const accId =14453 
-let accEmail ="a@gmail.com"
-var accPass ="12345"
-accountCity ="Jaipur"
+// const accId =14453 
+// let accEmail ="a@gmail.com"
+// var accPass ="12345"
+// accountCity ="Jaipur"
 
-console.log(typeof accEmail);
+// console.log(typeof accEmail);
 
-let email =Number(accEmail)
+// let email =Number(accEmail)
 
-console.log(typeof email);
+// console.log(typeof email);
 
 
-let isLog =0
+// let isLog =0
 
-let isLogBol =Boolean(isLog)
+// let isLogBol =Boolean(isLog)
 
-console.log(isLogBol)
+// console.log(isLogBol)
+
+
+// opreations
+
+// let value =3
+
+// let negValue = -value
+// console.log(negValue)
+
+// let str1 ="hello"
+
+// let str2 =" hitesh"
+
+// let str3 =str1 +str2
+// console.log(str3)
+
+// console.log("1"+ 2);
+// console.log(1 +"2");
+// console.log("1"+2+2);
+// console.log(1+2+"3");
+
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// // Expected output: "a:4, b:4"
