32cc8ecd880a73da445f76d2a424b77d04b48547
Atharva Mangesh Deshmukh <atharvadeshmukh0024@gmail.com>
2025-08-02 07:45:52 +0000

    datatype primitive and non  primitive

---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,22 @@
+// Primitive
+
+//  7 types :String ,Number ,Bollean ,Null ,undefined,Symbol,BigInt
+//Symbol
+// const id =Symbol('123')
+// console.log(id);
+
+// Non Primitive (Refrence)
+
+const alphabet =['A','B','C','D','E'];
+
+let myObj ={
+    naav :"atharva",
+    age:22,
+}
+
+const myFun =function(){
+    console.log("hello world");
+}
+
+console.log(typeof myObj)
+
