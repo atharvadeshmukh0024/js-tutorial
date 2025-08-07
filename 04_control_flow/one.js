6dcb89922994602c1051de27b785f4f095f40f5a
Atharva Mangesh Deshmukh <atharvadeshmukh0024@gmail.com>
2025-08-07 15:14:08 +0000

    done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,41 @@
+ // if 
+// comparison opreator = <,>,<=,>=,==,!=,==
+ const isUserLoggedIn =true
+
+//  if (isUserLoggedIn){
+//     console.log("executed")    //executed
+
+//  }
+
+//  if (2 != 2){
+//     console.log("executed")  // not executed means nothing print
+
+//  }
+
+//  if (2 != 3){
+//     console.log("executed")  //  executed 
+
+//  }
+
+// const temp =41
+
+//  if (temp ===41){
+//     console.log("less than 50");
+//  }else{
+//     console.log("more than 50")
+//  }
+
+
+const score =200
+
+// if(score>100){
+//     const power ="fly"
+//     console.log(`User power :${power}`);  //User power :fly
+// }
+
+
+if(score>100){
+    const power ="fly"
+    console.log(`User power :${power}`);  //User power :fly
+}
+console.log(`User power :${power}`);   //power is not defined as it is outside the scope
