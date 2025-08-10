065787730c2c9b5d333709521bee23e5b2928923
Atharva Mangesh Deshmukh <atharvadeshmukh0024@gmail.com>
2025-08-10 16:42:17 +0000

    done

---
+++ 05_itrrations/six.js
@@ -0,0 +1,105 @@
+// for each doesnt return any value 
+const programing =["js","rb" ,"py","java","cpp"]
+
+// const values =programing.forEach((item) =>{
+//     return item //undefined
+// })
+
+// console.log(values)
+
+const myNums =[1,2,3,4,5,6,7,8,9,10]
+// const newNums =myNums.filter((num) => num >4)  
+// console.log(newNums)  //[ 5, 6, 7, 8, 9, 10 ]
+// filter returns values 
+
+// const newNums =myNums.filter((num) => {
+//     num >4}
+// )  
+
+// console.log(newNums)  // []
+// as it is out  of  scope it doesnot return anything 
+// so it is mandatory to use return
+
+// const newNums =myNums.filter((num) => {
+//     return num >4} 
+// )  
+
+// console.log(newNums)   //[ 5, 6, 7, 8, 9, 10 ]
+
+
+// const newNums =[]
+
+// myNums.forEach((num) =>{
+//     if (num>4){
+//         newNums.push(num)
+//     }
+// })
+
+// console.log(newNums)
+
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  let  userBooks =books.filter((bk)=> bk.genre =='History')
+
+//   console.log(userBooks)
+
+//   [
+//   {
+//     title: 'Book Three',
+//     genre: 'History',
+//     publish: 1999,
+//     edition: 2007
+//   },
+//   {
+//     title: 'Book Seven',
+//     genre: 'History',
+//     publish: 1986,
+//     edition: 1996
+//   }
+// ]
+
+
+// userBooks =books.filter((bk)=> {bk.genre >= 20001} )
+
+// console.log(userBooks) //[]  dosent print anything as we had not used return keyword 
+
+// userBooks =books.filter((bk)=> { return bk.publish >= 2000} )
+
+// console.log(userBooks);
+
+// [
+//   {
+//     title: 'Book Five',
+//     genre: 'Science',
+//     publish: 2009,
+//     edition: 2014
+//   },
+//   {
+//     title: 'Book Eight',
+//     genre: 'Science',
+//     publish: 2011,
+//     edition: 2016
+//   }
+// ]
+
+
+// return  value  when used return keyword
+
+userBooks =books.filter((bk)=> {
+     return bk.publish <= 2000 && bk.genre === "History"
+    
+} )
+
+console.log(userBooks);
+
