c8e80df5b56216b4deef4917c7772d83e67cabcb
Atharva Mangesh Deshmukh <atharvadeshmukh0024@gmail.com>
2025-08-10 15:32:07 +0000

    four

---
+++ 05_itrrations/four.js
@@ -0,0 +1,55 @@
+const myobj ={
+    'js':"javascript",
+    'cpp':"C++",
+     'rb':"ruby"
+}
+
+// for (const key in myobj) {
+//     console.log(key)
+    
+// }
+// print key
+// js
+// cpp
+// rb
+
+// for (const key in myobj) {
+//     console.log(myobj[key])
+    
+// }
+
+// javascript
+// C++
+// ruby
+
+const programing =["js","rb" ,"py","java","cpp"]
+
+// for (const key in programing){
+//     console.log(key)
+// }
+
+// 0
+// 1
+// 2
+// 3
+// 4
+
+// for (const key in programing){
+//     console.log(programing[key])
+// }
+
+// js
+// rb
+// py
+// java
+// cpp
+
+const map =new Map()
+map.set('IN',"India")
+map.set('USA',"unites State of America")
+map.set('Fr',"France")
+// nothing print as it  map is not itreable
+// for (const key in map) {
+//     console.log(key);
+    
+// }
