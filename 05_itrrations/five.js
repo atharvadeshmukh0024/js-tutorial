0c7c8a386016a5a6e860e8fd7049543150cbc4a6
Atharva Mangesh Deshmukh <atharvadeshmukh0024@gmail.com>
2025-08-10 16:14:15 +0000

    done for each

---
+++ 05_itrrations/five.js
@@ -0,0 +1,53 @@
+const programing =["js","rb" ,"py","java","cpp"]
+
+// syntax
+// array.forEach(element => {
+    
+// });
+
+// here in call back we donot ned to write name of fun
+
+// programing.forEach(function (val){
+//     console.log(val);
+// })
+
+// js
+// rb
+// py
+// java
+// cpp
+
+// function printMe(item){
+//     console.log(item);
+// }
+// programing.forEach(printMe)
+
+// js
+// rb
+// py
+// java
+// cpp
+
+// const myCoding =[
+//     {
+//         langname :"javascript",
+//         langFilename :"js"
+//     },
+//     {
+//         langname :"java",
+//         langFilename :"java"
+//     },
+//     {
+//         langname :"python",
+//         langFilename :"py"
+//     }
+// ]
+
+// myCoding.forEach((item)=>{
+//     console.log(item.langname);
+    
+// })
+
+// javascript
+// java
+// python
