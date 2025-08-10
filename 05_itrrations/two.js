b6a130c51e05faade9392b1b1d98666aa95abaf7
Atharva Mangesh Deshmukh <atharvadeshmukh0024@gmail.com>
2025-08-10 14:30:26 +0000

    DO WHILE

---
+++ 05_itrrations/two.js
@@ -0,0 +1,47 @@
+
+//SYNTAX
+
+// while (condition) {
+    
+// }
+
+// let index =0
+// while (index <=10) {
+//     console.log(`value of index is ${index}`)
+//     index =index +2
+    
+// }
+
+// let myArray =["A","B","C","D"]
+// let arr =0
+// while (arr < myArray.length) {
+//     console.log(`value is ${myArray[arr]}`)
+//     arr =arr +1
+// }
+
+
+// *************************DO WHILE ************
+
+//SYNTAX
+
+// do {
+    
+// } while (condition);
+
+
+
+
+// let score =1
+
+// do {
+//     console.log(`Score is ${score}`);
+//     score ++
+// }while(score <= 10)
+
+
+// let score =11
+
+// do {
+//     console.log(`Score is ${score}`);  //Score is 11
+//     score ++
+// }while(score <= 10)
