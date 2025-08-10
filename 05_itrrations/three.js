5dd1d275f105addda257ef45c172782ee3874ac7
Atharva Mangesh Deshmukh <atharvadeshmukh0024@gmail.com>
2025-08-10 14:49:47 +0000

    done

---
+++ 05_itrrations/three.js
@@ -0,0 +1,39 @@
+//for of = are array specific loop      
+//  ["","",""]
+// obj in array = [{}{}{}]
+
+const arr =[1,2,3,4,5]
+
+// for (const element of object) {
+    
+// } object here state that - kis cheez par lagana hai
+
+// for (const num of arr) {
+//     console.log(num);
+    
+// }
+
+// const greeting ="Hello World!"
+// for (const greet of greeting) {
+//     console.log(`Each char is ${greet}`)
+    
+// }
+
+// Each char is H
+// Each char is e
+// Each char is l
+// Each char is l
+// Each char is o
+// Each char is  
+// Each char is W
+// Each char is o
+// Each char is r
+// Each char is l
+// Each char is d
+// Each char is !
+
+//Maps
+
+const map =new Map()
+
+
